f87790c3068a863e648eb09aaad71ccdbba2348f
Alex Jones <alexjajones1@gmail.com>
2017-01-01 16:42:55 +0000

    Simplified application background.

--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,12 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
 import './app.css';
-import Stars from '../stars/stars';
 
 class App extends Component {
 
     render() {
         return (
             <div className="app">
-                <Stars/>
                 {this.props.children}
             </div>
         );
