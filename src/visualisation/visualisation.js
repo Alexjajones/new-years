bd8233b8b88c53fc12f3b50fb0a2bfabca63abd5
Alex Jones <alexjajones1@gmail.com>
2017-12-31 17:31:02 +0000

    Added url to vis.

--- src/visualisation/visualisation.js
+++ src/visualisation/visualisation.js
@@ -10,6 +10,7 @@ class Visualisation extends Component {
             <div className="container">
                 <YouTube/>
                 <ShoutOuts/>
+                <div style="position: absolute;left: 30px; bottom: 30px; font-size: 20px;">http://bit.do/fairfield18</div>
             </div>
         );
     }
